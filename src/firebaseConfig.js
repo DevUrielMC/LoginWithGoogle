12bd1390a9dafaef1e22a8a9d90ab0f820507e28
DevUrielMC <urimarcervantes.95@gmail.com>
2019-12-26 16:07:41 -0600

    Login in React using the google API

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyD7LNIcx3tXQa8ouanXKMMFtdNTxvvzwps",
+    authDomain: "login-9bb99.firebaseapp.com",
+    databaseURL: "https://login-9bb99.firebaseio.com",
+    projectId: "login-9bb99",
+    storageBucket: "login-9bb99.appspot.com",
+    messagingSenderId: "287652373821",
+    appId: "1:287652373821:web:806aa2424ed1e6f2605154"
+
+}
